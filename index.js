d9944f0d96da0a87ed0ba84e96762acf9231a43b
Rippe330 <68754582+Rippe330@users.noreply.github.com>
2021-07-15 21:40:39 +0500

    Inital Upload

---
+++ index.js
@@ -0,0 +1,79 @@
+var noButtons = document.querySelectorAll(".drum").length;
+
+
+function soundOn (ident) {
+    switch (ident) {
+        case "w":
+        var audio = new Audio ('sounds/tom-1.mp3');
+        audio.play();
+        break;
+
+        case "a":
+            var audio = new Audio ('sounds/tom-2.mp3');
+            audio.play();
+        break;
+
+        case "s":
+            var audio = new Audio ('sounds/tom-3.mp3');
+            audio.play();
+        break;
+
+        case "d":
+            var audio = new Audio ('sounds/tom-4.mp3');
+            audio.play();
+        break;
+
+        case "j":
+            var audio = new Audio ('sounds/snare.mp3');
+            audio.play();
+        break;
+        
+        case "k":
+            var audio = new Audio ('sounds/crash.mp3');
+            audio.play();
+        break;
+
+        case "l":
+            var audio = new Audio ('sounds/kick-bass.mp3');
+            audio.play();
+        break;
+
+
+       }
+}
+    
+
+   
+
+for (var i = 0; i<noButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        soundOn(this.innerHTML);
+        buttonAnimation(this.innerHTML);
+       
+    });
+
+}
+
+document.addEventListener("keydown", function(event) {
+    soundOn(event.key);
+    buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+    var activeButton = ("." + currentKey);
+    document.querySelector(activeButton).classList.add("pressed");
+
+    setTimeout(function(){
+        document.querySelector(activeButton).classList.remove("pressed");
+
+     }, 100);
+    
+    
+
+}
+
+
+
+
+//
